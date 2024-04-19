4c4d931003c53e02f6b22befe4768aecf0395152
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-04-19 21:44:34 +0200

    Preparing adopt_dog component... not finished

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layouts/Layout";
+import CrudDog from "../components/CrudDog";
+import NotFound from "../components/NotFound";
 
 const router = createBrowserRouter([
     {
         path:'/',
-        element:<Layout/>
+        element:<Layout/>,
+        errorElement:<NotFound/>,
+        children:[
+            {
+                path:'/adopt_dog',
+                element:<CrudDog/>
+            }
+        ]
     }
 ])
 
