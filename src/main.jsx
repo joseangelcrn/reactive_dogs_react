4a7dbc0ef4429e75dabb01bcf3c03c038f8350c8
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-04-19 21:42:12 +0200

    Fonts roboto

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,12 @@ import router from "./router/index.jsx";
 import { Provider } from "react-redux";
 import store from "./redux/index.js";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
