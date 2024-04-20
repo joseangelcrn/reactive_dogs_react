46e30ba50e160018ba47118147f9b694270aa93c
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-04-20 23:31:48 +0200

    Refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,25 +8,26 @@ const Home = () => {
   const dogs = useSelector((store) => store.dog.data);
   const dispatch = useDispatch();
 
-  const fakeDogs = [];
-
   useEffect(() => {
-    //Generate 15 fake dogs 
-    dispatch(generateFakeDogs())
+    //Generate 15 fake dogs if not exists
+    // if (dogs.length === 0) {
+    //   dispatch(generateFakeDogs())
+    // }
+      // dispatch(generateFakeDogs())
+    
+    console.log(dogs);
   }, []);
 
   return (
-    <Grid container  columns={12} rowSpacing={6}>
-      {
-      dogs.map((dog) => (
-        <Grid key={dog.id} item xs={12} sm={6} md={4} lg={3}>
-          <DogPreview  dog={dog} />
-        </Grid>
-      ))
-  
-      }
+    <Grid container columns={12} rowSpacing={6}>
+      {dogs &&
+        dogs.map((dog) => (
+          <Grid key={dog.id} item xs={12} sm={6} md={4} lg={3}>
+            <DogPreview dog={dog} />
+          </Grid>
+        ))}
     </Grid>
-  );  
+  );
 };
 
 export default Home;
