d49aa96b1e28b97daca0404c36578537c764b4b4
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-04-26 00:21:11 +0200

    Change DogApi implementation

--- src/services/DogAPI.js
+++ src/services/DogAPI.js
@@ -1,19 +1,22 @@
-const dogAPI = {
-  url: "https://dog.ceo/api/breeds/image/random/",
+const url = "https://dog.ceo/api/breeds/image/random/";
 
-  async loadImages(n = 1) {
-    if (n > 50) {
-      n = 50;
-    }
-    let response = await fetch(this.url + n);
-    let data = await response.json();
+const loadImages = async (n = 1) => {
+  if (n > 50) {
+    n = 50;
+  }
+  let response = await fetch(url + n);
+  let data = await response.json();
 
-    if (data.status == 'success') {
-        return data.message;
-    }
-    console.error('Error in class DogAPI.js . Received status from api was not "success"')
-    return [];
-  },
+  if (data.status == "success") {
+    return data.message;
+  }
+  console.error(
+    'Error in class DogAPI.js . Received status from api was not "success"'
+  );
+  return [];
 };
 
-export default dogAPI;
+export default {
+  url,
+  loadImages,
+};
